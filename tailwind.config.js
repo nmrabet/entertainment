81de9b2e322dfb7bb7c5978e6a2079457e2647cf
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2022-03-23 20:40:50 +0100

    Starter files & design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      backblue: "#161D2F",
+      specialred: "#FC4747",
+      midnight: "#10141E",
+      metal: "#5A698F",
+    },
   },
   plugins: [],
-}
+};
