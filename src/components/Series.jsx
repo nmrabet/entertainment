6fff9de47f6453aa1d0a4e6fcee75ceb22a34f38
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2022-03-29 17:28:37 +0100

    Tv Shows filtered search

--- src/components/Series.jsx
+++ src/components/Series.jsx
@@ -1,35 +1,27 @@
 import React from "react";
-import data from "../data.json";
 import { ReactComponent as Bookmark } from "../assets/icon-bookmark-empty.svg";
 import { ReactComponent as TvIcon } from "../assets/icon-category-tv.svg";
 
-function Series() {
-  const series = data.filter((item) => item.category === "TV Series");
+function Series({ serie }) {
   return (
     <>
-      <h2 className="p-5 text-2xl">TV Series</h2>
       <div className="flex flex-wrap pl-5">
-        {series.map((item) => {
-          return (
-            <div key={item.title} className="relative">
-              <img
-                src={item.thumbnail.regular.small}
-                alt=""
-                className="rounded-xl mr-4 mb-2"
-              />
-              <Bookmark className="absolute top-5 right-10" />
-              <div className="mb-4">
-                <span>{item.year} &bull; </span>
-                <span>
-                  <TvIcon className="inline-block mb-1" /> {item.category}{" "}
-                  &bull;{" "}
-                </span>
-                <span>{item.rating}</span>
-                <p>{item.title}</p>
-              </div>
-            </div>
-          );
-        })}
+        <div key={serie.title} className="relative">
+          <img
+            src={serie.thumbnail.regular.small}
+            alt=""
+            className="rounded-xl mr-4 mb-2"
+          />
+          <Bookmark className="absolute top-5 right-10" />
+          <div className="mb-4">
+            <span>{serie.year} &bull; </span>
+            <span>
+              <TvIcon className="inline-block mb-1" /> {serie.category} &bull;{" "}
+            </span>
+            <span>{serie.rating}</span>
+            <p>{serie.title}</p>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/Tv.jsx
+++ src/pages/Tv.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import SearchBar from "../components/SearchBar";
 import Series from "../components/Series";
+import data from "../data.json";
 
 function Tv() {
+  const [series, setSeries] = useState("");
+
+  const tvShows = data.filter((item) => item.category === "TV Series");
+
+  const filteredSeries = tvShows.filter((movie) => {
+    return movie.title.toLocaleLowerCase().includes(series);
+  });
+
   return (
     <div className="lg:flex">
       <div>
         <Navbar />
       </div>
       <div>
-        <SearchBar placeholder="Search for TV series" />
-        <Series />
+        <SearchBar
+          placeholder="Search for TV series"
+          onChange={(e) => setSeries(e.target.value.toLocaleLowerCase())}
+        />
+        {series ? (
+          <div>
+            <h2 className="text-2xl pb-5 pl-5">
+              Found {filteredSeries.length} results for ‘{series}’
+            </h2>
+            {filteredSeries.map((serie) => {
+              return (
+                <div key={serie.title}>
+                  <Series serie={serie} />
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div>
+             <h2 className="p-5 text-2xl">TV Series</h2>
+            {tvShows.map((serie) => {
+              return (
+                <div key={serie.title}>
+                  <Series serie={serie} />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
